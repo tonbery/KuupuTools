f3444cc58cafe8eaac61965621c660074ddad6b9
Roni <tonbery@gmail.com>
2019-10-06 21:28:58 -0300

    Some tools and gitignore

---
+++ Assets/KuupuTools/Extensions/KuupuExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class KuupuExtensions
+{
+
+    //Transforms
+    public static void ResetTransform(this Transform transform)
+    {
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+    public static void ResetPosition(this Transform transform)
+    {
+        transform.localPosition = Vector3.zero;
+    }
+    public static void ResetRotation(this Transform transform)
+    {
+        transform.localRotation = Quaternion.identity;
+    }
+
+    public static void ResetScale(this Transform transform)
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+    public static void AlignToGround(this Transform tranform, LayerMask layerMask, float maxDistance)
+    {
+        Vector3 groundPoint = tranform.position;
+
+        KuupuTools.Raycast(tranform.position, Vector3.down, maxDistance, layerMask);
+
+        tranform.position = groundPoint;
+    }
+
+    //Distance
+    public static float Distance(this Vector3 from, Vector3 to) {
+        return (from - to).magnitude;
+    }       
+
+    //Directions
+    public static Vector3 Direction(this Vector3 from, Vector3 to)
+    {
+        return to - from;
+    }
+    public static Vector3 Direction(this Transform from, Transform to)
+    {
+        return to.position - from.position;
+    }
+
+    public static Vector3 Direction(this Transform from, Vector3 to)
+    {
+        return to - from.position;
+    }
+
+    public static Vector3 Direction(this Vector3 from, Transform to)
+    {
+        return to.position - from;        
+    }
+
+    //Vector operations
+    public static Vector3 ToPlanar(this Vector3 vector)
+    {
+        return new Vector3(vector.x, 0, vector.y);
+    }
+}
---
+++ Assets/KuupuTools/Extensions/KuupuTools.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class KuupuTools
+{
+    public static void ActivateGameObjects(GameObject[] objects, bool state)
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            objects[i].SetActive(state);
+        }
+    }
+
+    public static void ActivateGameObjects(MonoBehaviour[] objects, bool state)
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            objects[i].enabled = state;
+        }
+    }
+
+    public static RaycastHit Raycast(Vector3 start, Vector3 direction, float maxDistance, LayerMask mask)
+    {
+        Ray ray = new Ray(start, direction);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit, maxDistance, mask);
+
+        return hit;
+    }
+}
