bcec04be0b45ac508053e8924541e2d2ec49c23f
Roni <tonbery@gmail.com>
2019-10-23 13:39:01 -0300

    Generic pool

---
+++ KuupuTools/GenericPool/PoolController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolController:MonoBehaviour
+{
+    private static PoolController _instance;
+    public static PoolController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<PoolController>();
+                if (_instance == null)
+                {
+                    var no = new GameObject("PoolController");
+                    _instance = no.AddComponent<PoolController>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    Dictionary<int, List<GameObject>> _pools = new Dictionary<int, List<GameObject>>();
+    Dictionary<int, int> _objectsInUse = new Dictionary<int, int>();
+
+    public void CreatePoolChace(GameObject prefab)
+    {
+
+
+    }
+
+    public GameObject Request(GameObject prefab) 
+    {
+        var prefabID = prefab.GetInstanceID();
+        
+        if (!_pools.ContainsKey(prefabID))
+        {
+            _pools.Add(prefabID, new List<GameObject>());
+        }
+
+        if(_pools[prefabID].Count <= 0)
+        {
+            _pools[prefabID].Add(Instantiate(prefab));
+        }
+        
+        var obj = _pools[prefabID][0];
+        _pools[prefabID].Remove(obj);
+        _objectsInUse.Add(obj.GetInstanceID(), prefabID);
+        return obj;
+    }
+
+
+    public void ReturnToPool(GameObject instance)
+    {
+        var instanceID = instance.GetInstanceID();
+        if (!_objectsInUse.ContainsKey(instanceID)) {
+            Debug.LogError("Trying to return an object that is not from the pool");
+            return;
+        }
+
+        if(instance.transform.parent != null) instance.transform.SetParent(null);
+        instance.SetActive(false);
+
+        _pools[_objectsInUse[instanceID]].Add(instance);
+        _objectsInUse.Remove(instanceID);
+    }
+}
